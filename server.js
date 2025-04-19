bb948b9f69ff6891ad4a6b17de76d1b8c56972b9
My-Name-Hung <thanhhung11112002@gmail.com>
2025-04-19 23:38:08 +0700

    add bank contract9

--- server.js
+++ server.js
@@ -62,7 +62,18 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://mbbankvay.netlify.app/",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json({ limit: "50mb" })); // Increase limit for base64 images
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static(path.join(__dirname, "dist")));
